fb0109bbb4ba48fcdc61c7d2416267e277be576a
CiottiGiorgio <giorgio.ciotti@algorand.foundation>
2024-03-22 10:02:33 +0100

    Single contract Digital Marketplace.

--- projects/py-dm-intermediate-en/smart_contracts/digital_marketplace/contract.py
+++ projects/py-dm-intermediate-en/smart_contracts/digital_marketplace/contract.py
@@ -1,7 +1,131 @@
-from puyapy import ARC4Contract, arc4
+import puyapy as pup
+from puyapy import arc4
+
+FOR_SALE_MBR = 2_500 + 64 * 400
 
 
-class DigitalMarketplace(ARC4Contract):
-    @arc4.abimethod()
-    def hello(self, name: arc4.String) -> arc4.String:
-        return "Hello, " + name
+class DigitalMarketplace(arc4.ARC4Contract):
+    @arc4.abimethod
+    def allow_asset(
+        self, mbr_pay: pup.gtxn.PaymentTransaction, asset: pup.Asset
+    ) -> None:
+        _balance, opted_into = pup.op.AssetHoldingGet.asset_balance(
+            pup.Global.current_application_address, asset.asset_id
+        )
+        assert not opted_into
+
+        assert mbr_pay.receiver == pup.Global.current_application_address
+        assert mbr_pay.amount == pup.Global.asset_opt_in_min_balance
+
+        pup.itxn.AssetTransfer(
+            xfer_asset=asset,
+            asset_receiver=pup.Global.current_application_address,
+            asset_amount=0,
+        ).submit()
+
+    @arc4.abimethod
+    def first_deposit(
+        self,
+        mbr_pay: pup.gtxn.PaymentTransaction,
+        xfer: pup.gtxn.AssetTransferTransaction,
+        nonce: arc4.UInt64,
+        unitary_price: arc4.UInt64,
+    ) -> None:
+        assert mbr_pay.sender == pup.Txn.sender
+        assert mbr_pay.receiver == pup.Global.current_application_address
+        assert mbr_pay.amount == FOR_SALE_MBR
+
+        box_key = (
+            pup.Txn.sender.bytes + pup.op.itob(xfer.xfer_asset.asset_id) + nonce.bytes
+        )
+        _length, exists = pup.op.Box.length(box_key)
+        assert not exists
+
+        assert xfer.sender == pup.Txn.sender
+        assert xfer.asset_receiver == pup.Global.current_application_address
+        assert xfer.asset_amount > 0
+
+        pup.op.Box.put(box_key, pup.op.itob(xfer.asset_amount) + unitary_price.bytes)
+
+    @arc4.abimethod
+    def deposit(
+        self, xfer: pup.gtxn.AssetTransferTransaction, nonce: arc4.UInt64
+    ) -> None:
+        box_key = (
+            pup.Txn.sender.bytes + pup.op.itob(xfer.xfer_asset.asset_id) + nonce.bytes
+        )
+        _length, exists = pup.op.Box.length(box_key)
+        assert exists
+
+        assert xfer.sender == pup.Txn.sender
+        assert xfer.asset_receiver == pup.Global.current_application_address
+        assert xfer.asset_amount > 0
+
+        current_deposited = pup.op.btoi(pup.op.Box.extract(box_key, 0, 8))
+        pup.op.Box.replace(
+            box_key, 0, pup.op.itob(current_deposited + xfer.asset_amount)
+        )
+
+    @arc4.abimethod
+    def set_price(
+        self, asset: pup.UInt64, nonce: arc4.UInt64, unitary_price: arc4.UInt64
+    ) -> None:
+        box_key = pup.Txn.sender.bytes + pup.op.itob(asset) + nonce.bytes
+
+        pup.op.Box.replace(box_key, 8, unitary_price.bytes)
+
+    @arc4.abimethod
+    def withdraw(self, asset: pup.Asset, nonce: arc4.UInt64) -> None:
+        box_key = pup.Txn.sender.bytes + pup.op.itob(asset.asset_id) + nonce.bytes
+
+        current_deposited = pup.op.btoi(pup.op.Box.extract(box_key, 0, 8))
+        _deleted = pup.op.Box.delete(box_key)
+
+        pup.itxn.Payment(receiver=pup.Txn.sender, amount=FOR_SALE_MBR).submit()
+
+        pup.itxn.AssetTransfer(
+            xfer_asset=asset,
+            asset_receiver=pup.Txn.sender,
+            asset_amount=current_deposited,
+        ).submit()
+
+    @arc4.abimethod
+    def buy(
+        self,
+        owner: arc4.Address,
+        asset: pup.Asset,
+        nonce: arc4.UInt64,
+        buy_pay: pup.gtxn.PaymentTransaction,
+        quantity: arc4.UInt64,
+    ) -> None:
+        box_key = owner.bytes + pup.op.itob(asset.asset_id) + nonce.bytes
+
+        current_unitary_price = pup.op.btoi(pup.op.Box.extract(box_key, 8, 8))
+        amount_not_scaled_high, amount_not_scaled_low = pup.op.mulw(
+            current_unitary_price, quantity.decode()
+        )
+        scaling_factor_high, scaling_factor_low = pup.op.expw(10, asset.decimals)
+        _quotient_high, amount_to_be_paid, _remainder_high, _remainder_low = (
+            pup.op.divmodw(
+                amount_not_scaled_high,
+                amount_not_scaled_low,
+                scaling_factor_high,
+                scaling_factor_low,
+            )
+        )
+        assert not _quotient_high
+
+        assert buy_pay.sender == pup.Txn.sender
+        assert buy_pay.receiver.bytes == owner.bytes
+        assert buy_pay.amount == amount_to_be_paid
+
+        current_deposited = pup.op.btoi(pup.op.Box.extract(box_key, 0, 8))
+        pup.op.Box.replace(
+            box_key, 0, pup.op.itob(current_deposited - quantity.decode())
+        )
+
+        pup.itxn.AssetTransfer(
+            xfer_asset=asset,
+            asset_receiver=pup.Txn.sender,
+            asset_amount=quantity.decode(),
+        ).submit()
--- projects/py-dm-intermediate-en/tests/digital_marketplace_test.py
+++ projects/py-dm-intermediate-en/tests/digital_marketplace_test.py
@@ -1,16 +1,71 @@
+import base64
+import functools
+from copy import deepcopy
+
 import algokit_utils
+import algosdk
 import pytest
 from algokit_utils import get_localnet_default_account
 from algokit_utils.config import config
+from algosdk.atomic_transaction_composer import TransactionWithSigner
+from algosdk.transaction import (
+    AssetCreateTxn,
+    AssetTransferTxn,
+    PaymentTxn,
+    wait_for_confirmation,
+)
 from algosdk.v2client.algod import AlgodClient
 from algosdk.v2client.indexer import IndexerClient
 
-from smart_contracts.artifacts.digital_marketplace.client import DigitalMarketplaceClient
+from smart_contracts.artifacts.digital_marketplace.client import (
+    DigitalMarketplaceClient,
+)
+
+FOR_SALE_MBR = 2_500 + 64 * 400 - 1
+
+
+@pytest.fixture(scope="session")
+def test_assets_id(algod_client: AlgodClient) -> (int, int):
+    account = get_localnet_default_account(algod_client)
+
+    return (
+        wait_for_confirmation(
+            algod_client,
+            algod_client.send_transaction(
+                AssetCreateTxn(
+                    sender=account.address,
+                    sp=algod_client.suggested_params(),
+                    total=10_000,
+                    decimals=3,
+                    default_frozen=False,
+                ).sign(account.private_key)
+            ),
+        )["asset-index"],
+        wait_for_confirmation(
+            algod_client,
+            algod_client.send_transaction(
+                AssetCreateTxn(
+                    sender=account.address,
+                    sp=algod_client.suggested_params(),
+                    total=10_000,
+                    decimals=3,
+                    default_frozen=False,
+                ).sign(account.private_key)
+            ),
+        )["asset-index"],
+    )
+
+
+@pytest.fixture(scope="session")
+def creator(algod_client: AlgodClient) -> algokit_utils.Account:
+    return get_localnet_default_account(algod_client)
 
 
 @pytest.fixture(scope="session")
 def digital_marketplace_client(
-    algod_client: AlgodClient, indexer_client: IndexerClient
+    creator: algokit_utils.Account,
+    algod_client: AlgodClient,
+    indexer_client: IndexerClient,
 ) -> DigitalMarketplaceClient:
     config.configure(
         debug=True,
@@ -19,30 +74,303 @@ def digital_marketplace_client(
 
     client = DigitalMarketplaceClient(
         algod_client,
-        creator=get_localnet_default_account(algod_client),
+        creator=creator,
         indexer_client=indexer_client,
     )
 
-    client.deploy(
-        on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
-        on_update=algokit_utils.OnUpdate.AppendApp,
+    # client.deploy(
+    #     on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
+    #     on_update=algokit_utils.OnUpdate.AppendApp,
+    # )
+    client.create_bare()
+    algokit_utils.ensure_funded(
+        algod_client,
+        algokit_utils.EnsureBalanceParameters(
+            account_to_fund=client.app_address, min_spending_balance_micro_algos=0
+        ),
     )
     return client
 
 
-def test_says_hello(digital_marketplace_client: DigitalMarketplaceClient) -> None:
-    result = digital_marketplace_client.hello(name="World")
-
-    assert result.return_value == "Hello, World"
-
-
-def test_simulate_says_hello_with_correct_budget_consumed(
-    digital_marketplace_client: DigitalMarketplaceClient, algod_client: AlgodClient
+def test_allow_asset(
+    digital_marketplace_client: DigitalMarketplaceClient,
+    creator: algokit_utils.Account,
+    test_assets_id: (int, int),
 ) -> None:
-    result = (
-        digital_marketplace_client.compose().hello(name="World").hello(name="Jane").simulate()
-    )
+    for asset_id in test_assets_id:
+        pytest.raises(
+            algosdk.error.AlgodHTTPError,
+            functools.partial(
+                digital_marketplace_client.algod_client.account_asset_info,
+                digital_marketplace_client.app_address,
+                asset_id,
+            ),
+        )
 
-    assert result.abi_results[0].return_value == "Hello, World"
-    assert result.abi_results[1].return_value == "Hello, Jane"
-    assert result.simulate_response["txn-groups"][0]["app-budget-consumed"] < 100
+        sp = digital_marketplace_client.algod_client.suggested_params()
+        sp_call = deepcopy(sp)
+        sp_call.flat_fee = True
+        sp_call.fee = 2_000
+
+        result = digital_marketplace_client.allow_asset(
+            mbr_pay=TransactionWithSigner(
+                PaymentTxn(
+                    sender=creator.address,
+                    sp=sp,
+                    receiver=digital_marketplace_client.app_address,
+                    amt=100_000,
+                ),
+                creator.signer,
+            ),
+            asset=asset_id,
+            transaction_parameters=algokit_utils.TransactionParameters(
+                suggested_params=sp_call
+            ),
+        )
+
+        assert result.confirmed_round
+
+        assert (
+            digital_marketplace_client.algod_client.account_asset_info(
+                digital_marketplace_client.app_address, asset_id
+            )["asset-holding"]["amount"]
+            == 0
+        )
+
+
+def test_first_deposit(
+    digital_marketplace_client: DigitalMarketplaceClient,
+    creator: algokit_utils.Account,
+    test_assets_id: (int, int),
+) -> None:
+    for asset_id in test_assets_id:
+        box_key = (
+            algosdk.encoding.decode_address(creator.address)
+            + algosdk.encoding.encode_as_bytes(asset_id)
+            + algosdk.encoding.encode_as_bytes(0)
+        )
+
+        sp = digital_marketplace_client.algod_client.suggested_params()
+        sp_call = deepcopy(sp)
+        sp_call.flat_fee = True
+        sp_call.fee = 2_000
+
+        result = digital_marketplace_client.first_deposit(
+            mbr_pay=TransactionWithSigner(
+                PaymentTxn(
+                    sender=creator.address,
+                    sp=sp,
+                    receiver=digital_marketplace_client.app_address,
+                    amt=FOR_SALE_MBR,
+                ),
+                creator.signer,
+            ),
+            xfer=TransactionWithSigner(
+                AssetTransferTxn(
+                    sender=creator.address,
+                    sp=sp,
+                    receiver=digital_marketplace_client.app_address,
+                    amt=3_000,
+                    index=asset_id,
+                ),
+                creator.signer,
+            ),
+            nonce=0,
+            unitary_price=1_000_000,
+            transaction_parameters=algokit_utils.TransactionParameters(
+                boxes=[(0, box_key)]
+            ),
+        )
+
+        assert result.confirmed_round
+
+        box_content = digital_marketplace_client.algod_client.application_box_by_name(
+            digital_marketplace_client.app_id,
+            box_key,
+        )["value"]
+        decoded_box_content = base64.b64decode(box_content)
+        assert int.from_bytes(decoded_box_content[:8], "big") == 3_000
+        assert int.from_bytes(decoded_box_content[8:16], "big") == 1_000_000
+
+
+def test_deposit(
+    digital_marketplace_client: DigitalMarketplaceClient,
+    creator: algokit_utils.Account,
+    test_assets_id: (int, int),
+) -> None:
+    for asset_id in test_assets_id:
+        box_key = (
+            algosdk.encoding.decode_address(creator.address)
+            + algosdk.encoding.encode_as_bytes(asset_id)
+            + algosdk.encoding.encode_as_bytes(0)
+        )
+
+        sp = digital_marketplace_client.algod_client.suggested_params()
+        sp_call = deepcopy(sp)
+        sp_call.flat_fee = True
+        sp_call.fee = 2_000
+
+        result = digital_marketplace_client.deposit(
+            xfer=TransactionWithSigner(
+                AssetTransferTxn(
+                    sender=creator.address,
+                    sp=sp,
+                    receiver=digital_marketplace_client.app_address,
+                    amt=1_000,
+                    index=asset_id,
+                ),
+                creator.signer,
+            ),
+            nonce=0,
+            transaction_parameters=algokit_utils.TransactionParameters(
+                boxes=[(0, box_key)]
+            ),
+        )
+
+        assert result.confirmed_round
+
+        box_content = digital_marketplace_client.algod_client.application_box_by_name(
+            digital_marketplace_client.app_id,
+            box_key,
+        )["value"]
+        decoded_box_content = base64.b64decode(box_content)
+        assert int.from_bytes(decoded_box_content[:8], "big") == 4_000
+        assert int.from_bytes(decoded_box_content[8:16], "big") == 1_000_000
+
+
+def test_set_price(
+    digital_marketplace_client: DigitalMarketplaceClient,
+    creator: algokit_utils.Account,
+    test_assets_id: (int, int),
+) -> None:
+    for asset_id, unitary_price in zip(test_assets_id, [3_200_000, 5_700_000]):
+        box_key = (
+            algosdk.encoding.decode_address(creator.address)
+            + algosdk.encoding.encode_as_bytes(asset_id)
+            + algosdk.encoding.encode_as_bytes(0)
+        )
+
+        result = digital_marketplace_client.set_price(
+            asset=asset_id,
+            nonce=0,
+            unitary_price=unitary_price,
+            transaction_parameters=algokit_utils.TransactionParameters(
+                boxes=[(0, box_key)]
+            ),
+        )
+
+        assert result.confirmed_round
+
+        box_content = digital_marketplace_client.algod_client.application_box_by_name(
+            digital_marketplace_client.app_id,
+            box_key,
+        )["value"]
+        assert (
+            int.from_bytes(base64.b64decode(box_content)[8:16], "big") == unitary_price
+        )
+
+
+def test_buy(
+    digital_marketplace_client: DigitalMarketplaceClient,
+    creator: algokit_utils.Account,
+    test_assets_id: (int, int),
+) -> None:
+    buyer = algokit_utils.get_account(digital_marketplace_client.algod_client, "buyer")
+
+    for asset_id, amount_to_pay in zip(test_assets_id, [6_793_600, 12_101_100]):
+        box_key = (
+            algosdk.encoding.decode_address(creator.address)
+            + algosdk.encoding.encode_as_bytes(asset_id)
+            + algosdk.encoding.encode_as_bytes(0)
+        )
+
+        wait_for_confirmation(
+            digital_marketplace_client.algod_client,
+            digital_marketplace_client.algod_client.send_transaction(
+                AssetTransferTxn(
+                    sender=buyer.address,
+                    sp=digital_marketplace_client.algod_client.suggested_params(),
+                    receiver=buyer.address,
+                    amt=0,
+                    index=asset_id,
+                ).sign(buyer.private_key)
+            ),
+        )
+
+        sp = digital_marketplace_client.algod_client.suggested_params()
+        sp_call = deepcopy(sp)
+        sp_call.flat_fee = True
+        sp_call.fee = 2_000
+
+        result = digital_marketplace_client.buy(
+            owner=creator.address,
+            asset=asset_id,
+            nonce=0,
+            buy_pay=TransactionWithSigner(
+                PaymentTxn(
+                    sender=buyer.address,
+                    sp=sp,
+                    receiver=creator.address,
+                    amt=amount_to_pay,
+                ),
+                buyer.signer,
+            ),
+            quantity=2_123,
+            transaction_parameters=algokit_utils.TransactionParameters(
+                sender=buyer.address,
+                signer=buyer.signer,
+                boxes=[(0, box_key)],
+                suggested_params=sp_call,
+            ),
+        )
+
+        assert result.confirmed_round
+
+        assert (
+            digital_marketplace_client.algod_client.account_asset_info(
+                buyer.address, asset_id
+            )["asset-holding"]["amount"]
+            == 2_123
+        )
+
+
+def test_withdraw(
+    digital_marketplace_client: DigitalMarketplaceClient,
+    creator: algokit_utils.Account,
+    test_assets_id: (int, int),
+) -> None:
+    for asset_id in test_assets_id:
+        box_key = (
+            algosdk.encoding.decode_address(creator.address)
+            + algosdk.encoding.encode_as_bytes(asset_id)
+            + algosdk.encoding.encode_as_bytes(0)
+        )
+
+        before_call_amount = digital_marketplace_client.algod_client.account_info(
+            creator.address
+        )["amount"]
+
+        sp = digital_marketplace_client.algod_client.suggested_params()
+        sp.flat_fee = True
+        sp.fee = 3_000
+        result = digital_marketplace_client.withdraw(
+            asset=asset_id,
+            nonce=0,
+            transaction_parameters=algokit_utils.TransactionParameters(
+                boxes=[(0, box_key)], suggested_params=sp
+            ),
+        )
+
+        assert result.confirmed_round
+
+        after_call_amount = digital_marketplace_client.algod_client.account_info(
+            creator.address
+        )["amount"]
+
+        assert after_call_amount - before_call_amount == FOR_SALE_MBR - 3_000
+        assert (
+            digital_marketplace_client.algod_client.account_asset_info(
+                creator.address, asset_id
+            )["asset-holding"]["amount"]
+            == 7_877
+        )
